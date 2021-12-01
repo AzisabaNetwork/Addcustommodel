514b16b5b0f27f0e5461c3b17e581f8b5f97a7a8
46kuri_ <ghost.46kuri@gmail.com>
2021-12-01 15:06:28 +0900

    package名変更

---
+++ src/main/java/com/github/sirokuri_/addcustommodel/Addcustommodel.java
@@ -0,0 +1,22 @@
+package com.github.sirokuri_.addcustommodel;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Addcustommodel extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        getCommand("acm").setExecutor(new command(this));
+        getCommand("gcm").setExecutor(new command(this));
+        getCommand("macm").setExecutor(new command(this));
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
